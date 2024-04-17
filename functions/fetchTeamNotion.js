7ec75db6edb9b654ea30847fe53258e515a97ce9
duduvalera <duduvalera14@gmail.com>
2024-04-17 16:38:05 -0300

    Upload do template e busca das informações via API

---
+++ functions/fetchTeamNotion.js
@@ -0,0 +1,36 @@
+import { Client } from "@notionhq/client";
+
+const { NOTION_KEY } = process.env;
+
+// Initializing a client
+const notion = new Client({
+  auth: NOTION_KEY,
+})
+
+export const handler = async (event, context) => {
+    
+    const databaseId = '83aa83f3b6f145f3a00e676d75608f38';
+    try {
+        const response = await notion.databases.query({
+            database_id: databaseId,
+            filter: {
+                property: "Status",
+                select: {
+                    equals: "Participantes"
+                }
+            }
+        })
+    
+        return {
+          body: JSON.stringify(response),
+          statusCode: 200
+        };
+    } catch(e) {
+        console.error(e)
+        return {
+            statusCode: 500,
+            body: e.toString()
+        }
+    }
+  };
+  
--- main.js
+++ main.js
@@ -1,38 +1,26 @@
-import './style.css'
-import javascriptLogo from './javascript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.js'
 
-// document.querySelector('#app').innerHTML = `
-//   <div>
-//     <a href="https://vitejs.dev" target="_blank">
-//       <img src="${viteLogo}" class="logo" alt="Vite logo" />
-//     </a>
-//     <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
-//       <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
-//     </a>
-//     <h1>Hello Vite!</h1>
-//     <div class="card">
-//       <button id="counter" type="button"></button>
-//     </div>
-//     <p class="read-the-docs">
-//       Click on the Vite logo to learn more
-//     </p>
-//   </div>
-// `
+async function fetchTeamDataFromAPIEndpoint() {
+    const cards = await fetch('/api/fetchTeamNotion').then((res) => res.json().then((data) => data.results))
 
-// setupCounter(document.querySelector('#counter'))
-
-async function fetchDataFromAPIEndpoint() {
-    const cards = await fetch('/api/fetchNotion').then((res) => res.json().then((data) => data.results))
-
-    document.querySelector("#app").innerHTML = cards.map((card) => `
-        <article>
-            <img class="logo" src="${card.properties.Imagem.rich_text[0].href}" alt="Imagem de perfil de ${card.properties.Name.title[0].plain_text}" />
-            <h2>${card.properties.Name.title[0].plain_text} - ${card.properties.Email.rich_text[0].plain_text}</h2>
-            <p>${card.properties.Descricao.rich_text[0].plain_text}</p>
-        </article>
-    `).join('')
+    document.querySelector("#equipe-tech").innerHTML = cards.map((card, index) => `
+    <div class="col-xl-3 col-lg-4 col-md-6" data-aos="fade-up" data-aos-delay="100">
+              <div class="member">
+                <img src="${card.properties.Imagem.rich_text[0].plain_text}" class="img-fluid" alt="">
+                <div class="member-info">
+                  <div class="member-info-content">
+                    <h4>${card.properties.Name.title[0].plain_text}</h4>
+                    <span>${card.properties.Descricao.rich_text[0].plain_text}</span>
+                  </div>
+                  <div class="social">
+                    <a href=""><i class="bi bi-twitter"></i></a>
+                    <a href=""><i class="bi bi-facebook"></i></a>
+                    <a href=""><i class="bi bi-instagram"></i></a>
+                    <a href=""><i class="bi bi-linkedin"></i></a>
+                  </div>
+                </div>
+              </div>
+            </div><!-- End Team Member -->
+`).join('')
 }
 
-fetchDataFromAPIEndpoint()
+fetchTeamDataFromAPIEndpoint()
