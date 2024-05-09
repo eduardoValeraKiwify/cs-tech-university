5b3d867bf0534fe772f724aa62bf5ddc51093684
JoaoOtranto <joaootranto@gmail.com>
2024-05-09 15:05:23 -0300

    atividades correção

---
+++ public/assets/vendor/menuToggle.js
@@ -0,0 +1,19 @@
+function menuToggle1(){
+    var nav = document.getElementById('overlay-atv1');
+    nav.classList.toggle('active');
+}
+
+function menuToggle2(){
+    var nav = document.getElementById('overlay-atv2');
+    nav.classList.toggle('active');
+}
+
+function menuToggle3(){
+    var nav = document.getElementById('overlay-atv3');
+    nav.classList.toggle('active');
+}
+
+function menuToggle4(){
+    var nav = document.getElementById('overlay-atv4');
+    nav.classList.toggle('active');
+}
